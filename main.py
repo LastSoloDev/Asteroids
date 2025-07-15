964965f1b8c0a61ab67570e7a217797d588f4e61
LastSoloDev <ilia.katulin@gmail.com>
2025-07-15 22:51:58 +0200

    Optimised for 60FPS by using clock tick

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    # GAME LOOP START
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+
+        dt = clock.tick(60) / 1000
+
         pygame.display.flip()
 
 if __name__ == "__main__":
